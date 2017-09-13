06a7e7dba69412ca4de7c9ff9912a00015c98dd9
Vincent Liu <zl2474@columbia.edu>
2017-09-12 22:46:11 -0400

    implemented file loading window

--- src/scrb.py
+++ src/scrb.py
@@ -59,6 +59,76 @@ class SCRBGui(tk.Tk):
         self.focus_force()
 
     def load_files(self):
+        self.import_files = tk.Toplevel()
+        self.import_files.resizable(False, False)
+        self.import_files.title('Import files')
+
+        # get file path of count matrix
+        matrixContainer = tk.Frame(self.import_files)
+        matrixContainer.grid(column=0, row=0, sticky='w')
+        tk.Label(matrixContainer, text="Count matrix file: ").grid(column=0, row=0, sticky='w')
+        tk.Button(matrixContainer, text="Load", command=lambda: self.get_filename('count matrix')).grid(column=1,
+                                                                                                          row=0,
+                                                                                                          sticky='w')
+        self.matrixfileVar = tk.StringVar()
+        self.matrixfileVar.set("None selected")
+        tk.Label(matrixContainer, textvariable=self.matrixfileVar).grid(column=2, row=0, sticky='e')
+
+        # get file path of cluster
+        clusterContainer = tk.Frame(self.import_files)
+        clusterContainer.grid(column=0, row=1, sticky='w')
+        tk.Label(clusterContainer, text="Cluster file: ").grid(column=0, row=0, sticky='w')
+        tk.Button(clusterContainer, text="Load", command=lambda: self.get_filename('cluster')).grid(column=1, row=0,
+                                                                                                     sticky='w')
+        self.clusterfileVar = tk.StringVar()
+        self.clusterfileVar.set("None selected")
+        tk.Label(clusterContainer, textvariable=self.clusterfileVar).grid(column=2, row=0, sticky='e')
+
+        # get file path of tsne
+        tsneContainer = tk.Frame(self.import_files)
+        tsneContainer.grid(column=0, row=2, sticky='w')
+        tk.Label(tsneContainer, text="tSNE file: ").grid(column=0, row=0, sticky='w')
+        tk.Button(tsneContainer, text="Load", command=lambda: self.get_filename('tsne')).grid(column=1, row=0,
+                                                                                                  sticky='w')
+
+        self.tsnefileVar = tk.StringVar()
+        self.tsnefileVar.set("None selected")
+        tk.Label(tsneContainer, textvariable=self.tsnefileVar).grid(column=2, row=0, sticky='e')
+
+        # get file path of gene list
+        genelistContainer = tk.Frame(self.import_files)
+        genelistContainer.grid(column=0, row=3, sticky='w')
+        tk.Label(genelistContainer, text="Gene list file: ").grid(column=0, row=0, sticky='w')
+        tk.Button(genelistContainer, text="Load", command=lambda: self.get_filename('gene list')).grid(column=1, row=0,
+                                                                                                       sticky='w')
+        self.genelistfileVar = tk.StringVar()
+        self.genelistfileVar.set("None selected")
+        tk.Label(genelistContainer, textvariable=self.genelistfileVar).grid(column=2, row=0, sticky='e')
+
+        # final buttons
+        finalButtonContainer = tk.Frame(self.import_files)
+        finalButtonContainer.grid(column=0, row=4, sticky='w')
+        tk.Button(finalButtonContainer, text="Cancel", command=self.import_files.destroy).grid(column=0, row=0, padx=35)
+        tk.Button(finalButtonContainer, text="Process", command=self.process_data).grid(column=1, row=0, padx=35)
+
+        self.wait_window(self.import_files)
+
+    def get_filename(self, type: str):
+        if type == "count matrix":
+            self.countmatrix_file = filedialog.askopenfilename(title='Load count matrix file',
+                                                               initialdir='~/.magic/data')
+            self.matrixfileVar.set(self.countmatrix_file)
+        elif type == "cluster":
+            self.cluster_file = filedialog.askopenfilename(title='Load cluster file', initialdir='~/.magic/data')
+            self.clusterfileVar.set(self.cluster_file)
+        elif type == "tsne":
+            self.tsne_file = filedialog.askopenfilename(title='Load tsne file', initialdir='~/.magic/data')
+            self.tsnefileVar.set(self.tsne_file)
+        elif type == "gene list":
+            self.genelist_file = filedialog.askopenfilename(title='Load gene list file', initialdir='~/.magic/data')
+            self.genelistfileVar.set(self.genelist_file)
+
+    def process_data(self):
         pass  # to be implemented
 
     def save_plot(self):
